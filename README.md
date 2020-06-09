f7d269b54f18a484adc1776bf5335d334b7b9dc5
Laeng <Laeng@users.noreply.github.com>
2020-06-10 05:25:06 +0900

    완성!

---
+++ README.md
@@ -0,0 +1,13 @@
+#VoiceManager
+초간단 디스코드 음성 채팅방 자동 생성 봇
+
+###config.json
+```json
+{
+  "requestChannelId": "접속하면 음성 채팅방이 생기는 음성 채팅방의 ID",
+  "parentChannelId": "음성 채팅방이 생길 카테고리 ID",
+  "gameRunningRole": "유저가 게임 실행시 강조 역할을 하는 디스코드 역할 ID",
+  "gameTitleName": "강조할 게임 이름",
+  "token": "토큰"
+}
+```
---
+++ voicebot.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+const RequestChannelId = config.requestChannelId; //보이스 채널 신청 채널
+const ParentChannelId  = config.parentChannelId; //보이스 채널 생성 위치
+const GameRunningRole  = config.gameRunningRole; //게임 실행시 부여될 등급
+const GameTitleName    = config.gameTitleName;       //게임 이름
+
+const UserChannels = [];
+const GameUserList = [];
+
+client.on('ready', () => {
+});
+
+client.on("voiceStateUpdate", function(oldMember, newMember){
+    let userChannelIndex = UserChannels.findIndex(id => id === oldMember.channelID);
+
+    if (newMember.channelID === RequestChannelId) {
+        newMember.guild.channels.create(newMember.member.user.tag, {
+            type: "voice",
+            bitrate: "96000",
+            parent: newMember.guild.channels.cache.find(channel => channel.id === ParentChannelId)
+        }).then(r => {
+            r.createOverwrite(newMember.member, {
+                'MANAGE_CHANNELS': true
+            }).catch(console.error);
+
+            newMember.member.voice.setChannel(r.id);
+            UserChannels.push(r.id);
+        });
+    } else {
+        if (userChannelIndex !== -1) {
+            if (oldMember.channel.members.size <= 0) {
+                oldMember.channel.delete("user empty").then(channel => {
+                    UserChannels.slice(userChannelIndex, 1);
+                });
+            }
+        }
+    }
+});
+
+client.on("presenceUpdate", function(oldUser, newUser){
+    let gamingUser = GameUserList.findIndex(id => id === newUser.user.id);
+
+    if (gamingUser === -1) {
+        if (newUser.activities.findIndex(activity => activity.name.toLowerCase() === GameTitleName.toLowerCase()) !== -1) {
+            newUser.member.roles.add(newUser.guild.roles.cache.get(GameRunningRole)).then(role => {
+                GameUserList.push(newUser.user.id);
+            });
+        }
+    } else {
+        if (oldUser.activities.findIndex(activity => activity.name.toLowerCase() === GameTitleName.toLowerCase()) !== -1) {
+            oldUser.member.roles.remove(oldUser.guild.roles.cache.get(GameRunningRole)).then(role => {
+                GameUserList.slice(GameUserList.findIndex(id => id === newUser.user.id), 1);
+            });
+        }
+    }
+});
+
+
+client.login(config.token);
